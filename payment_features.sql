6485549f4cd0be289e8077fdfb688154825cfc06
Milad <milad@Milads-MacBook-Pro.local>
2022-03-30 17:30:56 +0430

    fixing payment features

--- payment_features.sql
+++ payment_features.sql
@@ -3,32 +3,33 @@ SELECT date
      , binary_user_id
      , platform
      , 'trade' AS action
-     , number_of_trades AS action_count
-     , CAST(closed_pnl_usd AS INT64) AS value
+     , SUM(number_of_trades) AS action_count
+     , SUM(CAST(closed_pnl_usd AS INT64)) AS value
      -- asset suspiciuncy
   FROM bi.trades
- WHERE date = '2022-01-01')
+ WHERE date = '2022-01-01'
+ GROUP BY date, binary_user_id, platform, action)
 , payments AS (
 SELECT date
      , binary_user_id
-  FROM(
-SELECT DATE(transaction_time) AS date
-     , binary_user_id
-     , SUM(IF(category IN ('Client Deposit','Payment Agent Deposit')
-             , amount_usd
-             , 0)) AS deposit_usd
-     , SUM(IF(category IN ('Client Withdrawal','Payment Agent Withdrawal')
-             , amount_usd
-             , 0)) AS withdrawal_usd
-     , SUM(IF(category IN ('Client Deposit','Payment Agent Deposit')
-             , 1
-             , 0)) AS deposit_count
-     , SUM(IF(category IN ('Client Withdrawal','Payment Agent Withdrawal')
-             , 1
-             , 0)) AS withdrawal_count   
-  FROM `business-intelligence-240201.bi.bo_payment_model`
- WHERE category IN ('Client Withdrawal', 'Payment Agent Withdrawal'
-                   , 'Client Deposit', 'Payment Agent Deposit')
-       AND DATE(transaction_time)>='2022-01-01'
- GROUP BY 1, 2
-  ) AS daily)
+     , 'none' AS platform
+     , action
+     , SUM(action_count) AS action_count
+     , SUM(value) AS value
+  FROM (
+        SELECT DATE(transaction_time) AS date
+             , binary_user_id
+             , CASE 
+                   WHEN category IN ('Client Deposit','Payment Agent Deposit') THEN 'deposit'
+                   WHEN category IN ('Client Withdrawal','Payment Agent Withdrawal') THEN 'withdrawal' 
+                   END AS action
+             , 1 AS action_count
+             , amount_usd AS value
+          FROM `business-intelligence-240201.bi.bo_payment_model`
+         WHERE category IN ('Client Withdrawal', 'Payment Agent Withdrawal'
+                          , 'Client Deposit', 'Payment Agent Deposit')
+               AND DATE(transaction_time) >= '2022-01-01') AS daily
+GROUP BY date, binary_user_id, platform, action)
+SELECT * FROM trades
+UNION ALL
+SELECT * FROM payments
