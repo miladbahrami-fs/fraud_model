7edd4b85b1cfc4471d7ca586e3d9c056de8057c9
Milad <milad@Milads-MacBook-Pro.local>
2022-04-19 15:18:55 +0430

    updating payment features

--- payment_features.sql
+++ payment_features.sql
@@ -5,27 +5,30 @@ SELECT date
      , 'trade' AS action
      , SUM(number_of_trades) AS action_count
      , SUM(CAST(closed_pnl_usd AS INT64)) AS value
-     -- asset suspiciuncy
+     -- action suspiciency
   FROM bi.trades
  WHERE date = '2022-01-01'
  GROUP BY date, binary_user_id, platform, action)
 , payments AS (
 SELECT date
      , binary_user_id
-     , 'none' AS platform
+     , platform
      , action
      , SUM(action_count) AS action_count
      , SUM(value) AS value
+     -- action suspiciency
   FROM (
         SELECT DATE(transaction_time) AS date
              , binary_user_id
-             , CASE 
-                   WHEN category IN ('Client Deposit','Payment Agent Deposit') THEN 'deposit'
-                   WHEN category IN ('Client Withdrawal','Payment Agent Withdrawal') THEN 'withdrawal' 
-                   END AS action
+             , CASE WHEN ds.offerings = 'deriv_mobile_multipliers' THEN 'Deriv Go'
+                    WHEN ds.offerings IS NULL THEN 'others'
+                    ELSE ds.offerings END AS platform
+             , CASE WHEN category IN ('Client Deposit','Payment Agent Deposit') THEN 'deposit'
+                    WHEN category IN ('Client Withdrawal','Payment Agent Withdrawal') THEN 'withdrawal' END AS action
              , 1 AS action_count
              , amount_usd AS value
-          FROM `business-intelligence-240201.bi.bo_payment_model`
+          FROM `business-intelligence-240201.bi.bo_payment_model` payment
+          LEFT JOIN bi.dict_source ds on ds.id = payment.source
          WHERE category IN ('Client Withdrawal', 'Payment Agent Withdrawal'
                           , 'Client Deposit', 'Payment Agent Deposit')
                AND DATE(transaction_time) >= '2022-01-01') AS daily
