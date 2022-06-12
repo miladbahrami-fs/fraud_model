bfc7678237bc1c88015ba367568ede5ace7850bb
Milad <milad@Milads-MacBook-Pro.local>
2022-06-12 16:20:35 +0430

    adding buis

--- fraud_detected_users.sql
+++ fraud_detected_users.sql
@@ -8,6 +8,12 @@ WITH loginids AS (
       FROM development.fraud_detected
      WHERE loginid LIKE '%@%'
 )
+, buis AS (
+    SELECT binary_user_id 
+      FROM development.fraud_detected
+      JOIN bi.user_profile ON CAST(user_profile.binary_user_id AS STRING) = fraud_detected.loginid
+     WHERE REGEXP_CONTAINS(loginid, r'^[0-9]{7,8}$')
+)
 SELECT DISTINCT bc.binary_user_id
   FROM loginids
   JOIN bi.bo_client bc ON bc.loginid = loginids.loginid
@@ -15,3 +21,6 @@ UNION DISTINCT
 SELECT DISTINCT bc.binary_user_id
   FROM emails
   JOIN bi.bo_client bc ON bc.email = emails.email
+UNION DISTINCT
+SELECT binary_user_id
+  FROM buis
